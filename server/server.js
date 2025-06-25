527fbbff64f069a7fc849d5bf7de8f67ad440817
YateeshaPappala <yateeshapappala@gmail.com>
2025-06-25 10:19:25 +0530

    modeified server.js

--- server/server.js
+++ server/server.js
@@ -11,6 +11,25 @@ const app = express();
 
 app.use(express.json());
 
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://jobboarddx.vercel.app'
+];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) {
+      const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  },
+  credentials: true
+}));
+
+
 app.get('/', (req, res) => {
   res.send('JobBoardX API is running...');
 });
